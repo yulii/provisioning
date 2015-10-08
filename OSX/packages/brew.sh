330dabbeb4eb370959e233f715c91874199f7e06
Ryo Yoneyama (@yulii) <yone.info@gmail.com>
2015-10-08 18:11:38 +0900

    install gimp

---
+++ OSX/packages/brew.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -eu
+
+CWD=$(pwd)
+SWD=$(cd $(dirname $0) && pwd)
+
+# trap
+trap 'echo -e "\nabort!" ; exit 1' 1 2 3 15
+
+. $SWD/../functions/*.sh
+
+# assertion {{{
+
+assert_brew_installed
+
+# }}}
+
+brew update
+brew install caskroom/cask/brew-cask
+brew cask search
---
+++ OSX/packages/gimp.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -eu
+
+CWD=$(pwd)
+SWD=$(cd $(dirname $0) && pwd)
+
+# trap
+trap 'echo -e "\nabort!" ; exit 1' 1 2 3 15
+
+. $SWD/../functions/*.sh
+
+# assertion {{{
+
+assert_brew_installed
+# TODO: cask
+
+# }}}
+
+brew update
+brew cask install gimp
+
